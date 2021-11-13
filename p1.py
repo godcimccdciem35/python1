b21a4d942140323ebc34586b5773035f44739ff6
Ksenia Gubareva <94219721+godcimccdciem35@users.noreply.github.com>
2021-11-13 18:26:52 +0200

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,21 @@
+3#Task1
+print('Hello world')
+#Task2
+print('I am glad to see you. My name is Ksenia. Discover the world!')
+#Task3
+length=int(input('length='))
+width=int(input('width='))
+print(length*width)
+#Task4
+x=int(input('x='))
+y=int(input('y='))
+print(x+y)
+print(x*y)
+print(x-y)
+print(x/y)
+#Task5
+radius=int(input('radius='))
+print(2*radius)
+print(2*3.14159*radius)
+print(3.14159*radius*radius)
+
