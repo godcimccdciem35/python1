fba69b42825b020ddbf79316302635f13fa80d0f
Ksenia Gubareva <94219721+godcimccdciem35@users.noreply.github.com>
2021-11-26 20:48:29 +0200

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,21 @@
+#Task1
+x = 123
+print(int(123))
+#Task2
+x = int(123)
+print(float(123))
+#Task3
+x = float(12.345)
+print(int(12.345))
+#Task4
+number = int(input('number='))
+print(number % 10_000)
+#Task5
+x = input('x=')
+print(int(x[0]) + int(x[1]) + int(x[2]))
+#Task6
+a = float(input('a='))
+b = float(input('b='))
+c = float(input('c='))
+p = float((a+b+c) / 2)
+print((p * (p-a) * (p-b) * (p-c)) ** 0.5)
